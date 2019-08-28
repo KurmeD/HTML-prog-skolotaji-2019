3e43931c0cda6ba8d53f7d893d4d4fecdf016751
KurmeD <54103875+KurmeD@users.noreply.github.com>
2019-08-28 12:39:41 +0300

    js-masivi1

--- masivs.js
+++ masivs.js
@@ -1,12 +1,13 @@
 let masivs = [];
-​function iegutPecIndeksa(i) {//getIndex(i)
+
+function iegutPecIndeksa(i) {//getIndex(i)
   return masivs[i];
 }
-​
+
 function mainitPecIndeksa(i, vertiba) {//setIndex
   masivs[i] = vertiba;
 }
-​
+
 function dzestPecIndeksa(i) {//removeIndex
   masivs = masivs.splice(i, 1);//mēģina izņemt vienu elementu vai mainīt vienu elementu... masīvā vieta n vērtība
 }
@@ -14,7 +15,6 @@ function dzestPecIndeksa(i) {//removeIndex
 function print(){
     console.log(masivs);
 }
-
 
 function atrastVertibasIndeksu(vertiba) {
     for (let i = 0; i < masivs.length; i++) {
